c4b8e33ba228a97631a865e8cdb1407d72fbbaef
portes1 <pedro.portes@hotmail.com>
2020-10-06 20:18:08 -0300

    Update Aluguel.sol

--- Aluguel.sol
+++ Aluguel.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.4.22 <0.7.0;
-import "./ERC20.sol";
+// import "./ERC20.sol";
 
     contract Aluguel {
         address payable proprietario; //  proprietario e locatario, pagáveis para o pagamento e posiveis reembolsos;
@@ -7,36 +7,66 @@ import "./ERC20.sol";
    
    struct Propriedade{ //struct defindo os atributos referentes a propriedade, pode ser casa ou ap;
         uint256 preco;
-        address locatarioAtual;
+        address payable locatarioAtual;
         bool disponivel;
    }
-    Propriedade casa; //inicializando uma casa do tipo propriedade, sendo uma struct, caso o proprietario possua mais de uma propriedade, poderiamos fazer um array;
+    Propriedade[5] casa; //inicializando uma casa do tipo propriedade, sendo uma struct, caso o proprietario possua mais de uma propriedade, poderiamos fazer um array;
     
     modifier proprietarioOnly() { //modificador que definira alguns itens somente acessados pelo proprietario;
-        require(msg.sender == proprietarioAddress);
+        require(msg.sender == proprietario);
         _;
     }
     
     // construtor do nosso contrato, definindo a casa como disponivel e o preço como 500;
     constructor() public {
-        proprietarioAddress = msg.sender;
-        casa.disponivel = true;
-        casa.preco = 500;
-       
+    proprietario = msg.sender;
+    for (uint i=0; i<8; i++) {
+        casa[i].disponivel = true;
+        casa[i].preco = 500;
     }
+}
     //definicao dos getters a seguir:
     
     //retorna se a propriedade esta ou nao disponivel;
-    function getDisponibilidadeCasa() view public returns(bool) {
-        return casa.disponivel;
+    function getDisponibilidadeCasa(uint _casa) view public returns(bool) {
+        return casa[_casa].disponivel;
     }
     
-    function getPreco() view public returns(uint256) {
-        return casa.preco;
+    function getPreco(uint _casa) view public returns(uint256) {
+        return casa[_casa].preco;
     }
     
-    function getLocatarioAtual() view public returns(address) {
-        return casa.locatarioAtual;
+    function getLocatarioAtual(uint _casa) view public returns(address) { 
+        return casa[_casa].locatarioAtual;
     }
+    
+    function setDisponibilidadeCasa(uint _casa, bool _novaDisponibilidade) proprietarioOnly public //caso tenha ficado disponível o endereco do locatario atual é zerado
+    {
+    casa[_casa].disponivel = _novaDisponibilidade;
+    if (_novaDisponibilidade) {
+        casa[_casa].locatarioAtual = address(0);
+        }
+    }
+ 
+    function setPriceOfFlat(uint _casa, uint256 _preco) proprietarioOnly public // definicao do preco da Propriedade casa
+    {
+    casa[_casa].preco = _preco; 
+    }
+    
+   function alugaCasa(uint _casa) public payable returns(uint256) {
+    locatario = msg.sender;
+    if (msg.value % casa[_casa].preco == 0 && msg.value > 0 && casa[_casa].disponivel == true) {
+        uint256 mesesPagos = msg.value / casa[_casa].preco;
+        casa[_casa].disponivel = false;
+        casa[_casa].locatarioAtual = locatario;
+        proprietario.transfer(msg.value);
+        return mesesPagos;
+    } else {
+        locatario.transfer(msg.value);
+        return 0;
+        }
+}   
+
+    
     
 }
